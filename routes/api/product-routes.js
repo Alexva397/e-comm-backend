2e06b4d50a230d4037aefef8b4e77a8d9ec94728
Alexander Vadeboncoeur <asvadeboncoeur@gmail.com>
2021-05-05 01:28:35 -0500

    added categories and tags to find all query, finished find by id query

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -8,7 +8,9 @@ router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   try {
-    const productData = await Product.findAll();
+    const productData = await Product.findAll({
+      include: [{ model: Category, as: 'category' }, { model: Tag, as: 'tags' }]
+    });
     res.status(200).json(productData);
   } catch (err) {
     res.status(500).json(err);
@@ -21,7 +23,7 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findByPk(req.params.id, {
-      // include: [{ model:  }]
+      include: [{ model: Category, as: 'category' }, { model: Tag, as: 'tags' }]
     });
     if (!productData) {
       res.status(404).json({ message: 'This id does not match any product.' });
