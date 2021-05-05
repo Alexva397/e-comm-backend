b95806f50f75d4d2ca21b274b9cea18f6bc52c3b
Alexander Vadeboncoeur <asvadeboncoeur@gmail.com>
2021-05-05 01:48:39 -0500

    added my req.params.id to tag-routes, created get all and by id queries for category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,15 +3,33 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+  try {
+    const categoryData = await Category.findAll({
+      include: [{ model: Product, as: 'products' }]
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product, as: 'products' }]
+    });
+    if (!productData) {
+      res.status(404).json({ message: 'This id does not match any category.' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
+
+// TODO
 
 router.post('/', (req, res) => {
   // create a new category
