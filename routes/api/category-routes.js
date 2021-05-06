747a7aef2199cef9952e0fbe582097da354955a7
Alexander Vadeboncoeur <asvadeboncoeur@gmail.com>
2021-05-05 23:49:40 -0500

    created pst routes for tags and categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
-
 router.get('/', async (req, res) => {
   try {
     const categoryData = await Category.findAll({
@@ -29,10 +28,13 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-// TODO
-
-router.post('/', (req, res) => {
-  // create a new category
+router.post('/', async (req, res) => {
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
